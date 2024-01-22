7314a6a872ae23c8dab9e07ec1bdc80fd39963f7
SajariS <santeri.sajari@gmail.com>
2024-01-22 13:04:22 +0200

    Alustus

---
+++ src/main/java/RW/JuomaPeli/JuomaPeliApplication.java
@@ -0,0 +1,13 @@
+package RW.JuomaPeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuomaPeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuomaPeliApplication.class, args);
+	}
+
+}
