14e06835f7811daede342ab7c4d7504cdc6846a7
SajariS <santeri.sajari@gmail.com>
2024-03-20 18:51:27 +0200

    TurnDTO aloitettu

--- src/main/java/RW/JuomaPeli/domain/Character.java
+++ src/main/java/RW/JuomaPeli/domain/Character.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -29,7 +30,7 @@ public class Character {
 	private List<Card> card;
 	
 	@JsonIgnore
-	@OneToOne
+	@OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
 	@JoinColumn(name="playerId")
 	private Player player;
 
--- src/main/java/RW/JuomaPeli/domain/CharacterRepository.java
+++ src/main/java/RW/JuomaPeli/domain/CharacterRepository.java
@@ -3,5 +3,5 @@ package RW.JuomaPeli.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CharacterRepository extends CrudRepository<Character, Long> {
-
+	Character findByPlayer(Player player);
 }
